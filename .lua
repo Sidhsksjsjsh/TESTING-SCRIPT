4ba78e90a2bacfbc103b320e0835b39511fc09c5
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-25 11:14:58 +0900

    Update .lua

--- .lua
+++ .lua
@@ -23,19 +23,19 @@ ID1.Parent = screenGui
 
 local ID2 = Instance.new("TextBox")
 ID2.Size = UDim2.new(0, 150, 0, 20)
-ID2.Position = UDim2.new(0, 25, 0, 30)
+ID2.Position = UDim2.new(0, 25, 0, 55)
 ID2.PlaceholderText = "1"
 ID2.Parent = screenGui
 
 local ID3 = Instance.new("TextBox")
 ID3.Size = UDim2.new(0, 150, 0, 20)
-ID3.Position = UDim2.new(0, 25, 0, 20)
+ID3.Position = UDim2.new(0, 25, 0, 65)
 ID3.PlaceholderText = "2"
 ID3.Parent = screenGui
 
 local ID4 = Instance.new("TextBox")
 ID4.Size = UDim2.new(0, 150, 0, 20)
-ID4.Position = UDim2.new(0, 25, 0, 10)
+ID4.Position = UDim2.new(0, 25, 0, 75)
 ID4.PlaceholderText = "speed"
 ID4.Parent = screenGui
 
@@ -46,9 +46,9 @@ playButton.Text = "Play"
 playButton.Parent = screenGui
 
 local Anim = Instance.new("Animation")
-local track = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
 
 playButton.MouseButton1Click:Connect(function()
+local track = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
 if playButton.Text == "Play" then
       Anim.AnimationId = "rbxassetid://" .. tostring(ID1.Text)
       track:Play(ID2.Text,ID3.Text,ID4.Text)
